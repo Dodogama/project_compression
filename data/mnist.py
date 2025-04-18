26c5c4d4f4c8350b1b3d6e30af6e8ccae11032bb
Conrad Li <conradli90@gmail.com>
2025-04-17 20:18:01 -0400

    updating mnist datapipeline

--- data/mnist.py
+++ data/mnist.py
@@ -20,14 +20,18 @@ def get_mnist_pipeline(batch_size: int=32, val_split: float=0.2, exclude: set=No
         tuple: A tuple containing the training DataLoader and the testing DataLoader.
     """
     # Define the transformations to be applied to the images
-    transform = transforms.Compose([
+    transform_train = transforms.Compose([
         transforms.RandomAffine(degrees=0, translate=(2/28, 2/28)),
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
     ])
+    transform_eval = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
     # Load the MNIST training dataset
-    train_set = torchvision.datasets.MNIST(root=f'{PROJECT_DIR}/data', train=True, download=True, transform=transform)
-    test_set = torchvision.datasets.MNIST(root=f'{PROJECT_DIR}/data', train=False, download=True, transform=transform)
+    train_set = torchvision.datasets.MNIST(root=f'{PROJECT_DIR}/data', train=True, download=True, transform=transform_train)
+    test_set = torchvision.datasets.MNIST(root=f'{PROJECT_DIR}/data', train=False, download=True, transform=transform_eval)
     # class exclusion
     if exclude is not None:
         train_idx = [i for i in range(len(train_set)) if train_set.targets[i] not in exclude]
@@ -35,16 +39,16 @@ def get_mnist_pipeline(batch_size: int=32, val_split: float=0.2, exclude: set=No
         train_set = Subset(train_set, train_idx)
         test_set = Subset(test_set, test_idx)
     # split train validation
-    idx = list(range(len(train_set)))
+    idx = list(range(len(test_set)))
     idx = np.random.permutation(idx)
     split = int(val_split * len(train_set))
     train_idx, val_idx = idx[split:], idx[:split]
-    train_subset = Subset(train_set, train_idx)
-    val_subset = Subset(train_set, val_idx)
+    test_subset = Subset(test_set, train_idx)
+    val_subset = Subset(test_set, val_idx)
     # loaders
-    train_loader = DataLoader(train_subset, batch_size=batch_size, shuffle=True)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_subset, batch_size=batch_size, shuffle=False)
-    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_subset, batch_size=batch_size, shuffle=False)
     return train_loader, val_loader, test_loader
 
 
